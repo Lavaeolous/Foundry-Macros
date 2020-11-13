11fd55c9a4e7ccc818c2afd73f637bb74fd5caa6
Lavaeolous <fehling@uni-wuppertal.de>
2020-11-13 11:45:03 +0100

    Update foundry-teleportal.js

--- Macros/foundry-teleportal.js
+++ Macros/foundry-teleportal.js
@@ -1,7 +1,10 @@
 /* Lavaeolous – Teleportal Macro
  * 
+ * Change-Log:
+ * - 13.11.2020: Now only teleports that are at most one space away
+ * 
  * Bugs / Stuff to do and fix:
- * Teleports all owned tokens, not only the ones near the teleportal
+ * 
  * Animates the movement instead of "teleporting"
  * Doesn't pan the camera to the target
  * Has a hardcoded gridUnit instead of taking the one from the scene
@@ -10,37 +13,32 @@
  * Could use a better dialog interface
  */
 
- // Get the Teleportal Data
- var tokens = canvas.tokens.controlled[0];
- var ownedTokens = canvas.tokens.placeables.filter(t => t.owner && t.name.search(/Source/i) === -1 && t.name.search(/Teleportal/i) === -1)
- console.log("ownedTokens:");
- console.log(ownedTokens);
+// Variables
+var gridUnit = 128;
 
- const gridUnit = 128;
+var validRotations = {
+    "0": "N",
+    "45": "NE",
+    "90": "E",
+    "135": "SE",
+    "180": "S",
+    "225": "SW",
+    "270": "W",
+    "315": "NW"
+}
 
- var validRotations = {
-     "45": "N",
-     "90": "NE",
-     "135": "E",
-     "180": "SE",
-     "225": "S",
-     "270": "SW",
-     "315": "W",
-     "0": "NW"
- }
- 
- var validDirections = {
-     "N": "45",
-     "NE": "90",
-     "E": "135",
-     "SE": "180",
-     "S": "225",
-     "SW": "270",
-     "W": "315",
-     "NW": "0"
- }
+var validDirections = {
+    "N": "0",
+    "NE": "45",
+    "E": "90",
+    "SE": "135",
+    "S": "180",
+    "SW": "225",
+    "W": "270",
+    "NW": "315"
+}
 
- var validTargetOffset = {
+var validTargetOffset = {
     0: [
         0,
         0
@@ -58,80 +56,126 @@
         gridUnit
     ],
     4: [
-        2*gridUnit,
+        2 * gridUnit,
         0
     ],
     5: [
         gridUnit,
-        2*gridUnit
+        2 * gridUnit
     ]
 }
- 
- var validTargets = {};
- 
- var currentPosition = 0;
- 
- if (!tokens) ui.notifications.warn("Please select a valid teleportal!");
- else {
- 
-     var teleportalName= tokens.data.name;
-     var teleportalToken = canvas.tokens.placeables.find(t => t.name === teleportalName);
- 
-     console.log("teleportalName: " + teleportalName);
-     var source = teleportalName.match(/Source\[([^\]]*)/)[1];
-     console.log("source: " + source);
-     var targetPairs = teleportalName.match(/Targets\[([^\]]*)/)[1].split(",");
-     console.log("targetPairs: " + targetPairs);
- 
-     targetPairs.forEach (targetPair => {
-         console.log("targetPair: " + targetPair);
-         let direction = targetPair.split("_")[0];
-         let target = targetPair.split("_")[1];
-         validTargets[direction] = target;
-     });
- 
-     console.log("validTargets:");
-     console.log(validTargets);
-     
-     currentPosition = teleportalToken.data.rotation;
- 
-     createDialog();
- 
- }
- 
- // Create the dialog
- function createDialog() {
-     
-     let teleportalSettings = function (ownedTokens) {
-         console.log("ownedTokens in teleportalsettings");
-         console.log(ownedTokens);
 
+var validTargets = {};
+var teleportalCurrentAngle = 0;
+var teleportalToken;
+var teleportalName = "";
+var validOwnedTokens = {};
+
+// Initialize the Macro
+main();
+
+function main() {
+    initializeTeleportal();
+    getValidOwnedTokens();
+    if(Object.keys(validOwnedTokens).length > 0) {
+        createDialog();
+    } else {
+        ui.notifications.warn("No one is near enough to activate the teleportal.");
+    }
+    
+}
+
+function initializeTeleportal() {
+    // Get the currently selected teleportal
+    var teleportal = canvas.tokens.controlled[0];
+
+    if (!teleportal) ui.notifications.warn("Please select a valid teleportal!");
+    else {
+
+        /* Extract Teleportal Data
+         * Source: The Teleportal that is being activated
+         * TargetPairs: The valid Targets in the format "<direction>_<targetTeleportal(s)>"
+         */
          
-         let tokenNames = "";
-         let tokenIndex = 1;
-         ownedTokens.forEach(token => {
-             console.log("token.data.name: " + token.data.name);
-             console.log("index: " + tokenIndex);
-             console.log("ownedTokens.length: " + ownedTokens.length);
-             
-             tokenNames = tokenNames.concat("<strong>" + token.data.name + "</strong>");
-             if (tokenIndex < ownedTokens.length-1) {
-                 tokenNames = tokenNames.concat(", ");
-                 tokenIndex++;
-             } else if (tokenIndex == ownedTokens.length-1) {
-                 tokenNames = tokenNames.concat(" and ");
-                 tokenIndex++;
-             }
-             
-         });
-         if (tokenIndex > 1) {
-             tokenNames = tokenNames.concat(" are near enough to activate it.");
-         } else {
-             tokenNames = tokenNames.concat(" is near enough to activate it.")
-         }
-         console.log("tokenNames: " + tokenNames);
+        teleportalName = teleportal.data.name;
+        teleportalToken = canvas.tokens.placeables.find(t => t.name === teleportalName);
 
-         return `
+        var targetPairs = teleportalName.match(/Targets\[([^\]]*)/)[1].split(",");
+
+        targetPairs.forEach(targetPair => {
+            let direction = targetPair.split("_")[0];
+            let target = targetPair.split("_")[1];
+            validTargets[direction] = target;
+        });
+
+        teleportalCurrentAngle = teleportalToken.data.rotation;
+
+    }
+}
+
+function getValidOwnedTokens() {
+    
+    // Token Positions start at the top left corner of the token
+    let teleportalData = {
+        "x": teleportalToken.data.x,
+        "y": teleportalToken.data.y,
+        "width": teleportalToken.data.width * gridUnit,
+        "height": teleportalToken.data.height * gridUnit,
+    };
+
+    let validTeleportalZone = {
+        "x1": teleportalData.x - gridUnit,
+        "x2": teleportalData.x + teleportalData.width + gridUnit,
+        "y1": teleportalData.y - gridUnit,
+        "y2": teleportalData.y + teleportalData.height + gridUnit
+    };
+
+    // Get the owned tokens of the activating player
+    let ownedTokens = canvas.tokens.placeables.filter(t => t.owner && t.name.search(/Source/i) === -1 && t.name.search(/Teleportal/i) === -1)
+
+    ownedTokens.forEach(token => {
+
+        // Check if an owned token lies in the valid teleportal zone
+        if(token.data.x >= validTeleportalZone.x1 && token.data.x < validTeleportalZone.x2 && token.data.y >= validTeleportalZone.y1 && token.data.y < validTeleportalZone.y2) {
+
+            let currentNumberOfValidTokens = Object.keys(validOwnedTokens).length;
+
+            validOwnedTokens[currentNumberOfValidTokens+1] = token;
+
+        } else {
+            console.log("NO TOKEN IN REACH");
+        }
+    });
+}
+
+// Create the dialog
+function createDialog() {
+
+    let teleportalSettings = function () {
+
+        let tokenNames = "";
+        let tokenIndex = 1;
+        let validOwnedTokenKeys = Object.keys(validOwnedTokens);
+        validOwnedTokenKeys.forEach(key => {
+
+            tokenNames = tokenNames.concat("<strong>" + validOwnedTokens[key].data.name + "</strong>");
+            if (tokenIndex < validOwnedTokenKeys.length - 1) {
+                tokenNames = tokenNames.concat(", ");
+                tokenIndex++;
+            } else if (tokenIndex == validOwnedTokenKeys.length - 1) {
+                tokenNames = tokenNames.concat(" and ");
+                tokenIndex++;
+            }
+
+        });
+
+        if (tokenIndex > 1) {
+            tokenNames = tokenNames.concat(" are near enough to activate it.");
+        } else {
+            tokenNames = tokenNames.concat(" is near enough to activate it.")
+        }
+
+        return `
          <div class="main">
              <div class="headline">
                  <em>You see a heavy stone disk with large handles and a single straight indentation on top</em>
@@ -141,60 +185,58 @@
              </div>
          </div>
          `
-     }
- 
-     let teleportalButtons = {
-         99: {
-             label: "Use teleportal",
-             callback: () => useTeleportal()
-         },
-     };
- 
-     let validTargetIndex = 0;
- 
-     let validTargetKeys = Object.keys(validTargets);
-     validTargetKeys.forEach(key => {
- 
-         console.log("key: " + key);
-         console.log("validTargetIndex: " + validTargetIndex);
-         console.log("validRotation: " + validDirections[key]);
- 
-         teleportalButtons[validTargetIndex] = {
-             label: key,
-             callback: () => {
-                 setTeleportal(validDirections[key]);
-                 teleportalDialog.render(true);
-             }
-         }
- 
-         validTargetIndex++;
- 
-     })
- 
-     console.log("teleportalButtons:");
-     console.log(teleportalButtons);
+    }
+
+    let teleportalButtons = {
+        99: {
+            label: "Use teleportal",
+            callback: () => useTeleportal()
+        },
+    };
+
+    let validTargetIndex = 0;
+
+    let validTargetKeys = Object.keys(validTargets);
+    validTargetKeys.forEach(key => {
+
+        teleportalButtons[validTargetIndex] = {
+            label: key,
+            callback: () => {
+                setTeleportal(validDirections[key]);
+                teleportalDialog.render(true);
+            }
+        }
+
+        validTargetIndex++;
+
+    })
 
     let teleportalDialog = new Dialog({
         title: "Teleportal",
-        content: teleportalSettings(ownedTokens),
+        content: teleportalSettings(),
         buttons: teleportalButtons,
     }).render(true);
 };
- 
+
 // Set the teleportal disk to a specified rotation
 function setTeleportal(angle) {
-    currentPosition = angle;
+    teleportalCurrentAngle = angle;
     teleportalToken.data.rotation = angle;
-    teleportalToken.update({"data.rotation":angle});
+    teleportalToken.update({ "data.rotation": angle });
 }
- 
+
 // Use the teleportal
 function useTeleportal() {
+    /*
     console.log("using the teleportal");
-    console.log("currentPosition: " + currentPosition);
-    console.log("correspondingDirection: " + validRotations[currentPosition]);
-    console.log("target: " + validTargets[validRotations[currentPosition]]);
-    let target = validTargets[validRotations[currentPosition]];
+    console.log("teleportalCurrentAngle: " + teleportalCurrentAngle);
+    console.log("correspondingDirection: " + validRotations[teleportalCurrentAngle]);
+    console.log("target: " + validTargets[validRotations[teleportalCurrentAngle]]);
+    */
+
+    // Fix for ppl changing the angle per alt-mousewheel after picking a direction
+    teleportalCurrentAngle = teleportalToken.data.rotation;
+    let target = validTargets[validRotations[teleportalCurrentAngle]];
 
     // Search a valid target token in the scene
     searchTeleportal(target);
@@ -202,44 +244,39 @@ function useTeleportal() {
 
 // Search for the target teleportal in the scene
 function searchTeleportal(target) {
-    console.log("Searching for " + target);
 
-    let regExpString = "Source\\["+target+"\\].*";
-    let targetRegExp = new RegExp (regExpString,"i")
-
-    console.log("targetRegExp: " + targetRegExp);
+    let regExpString = "Source\\[" + target + "\\].*";
+    let targetRegExp = new RegExp(regExpString, "i")
 
     try {
-        let targetTeleportal = canvas.tokens.placeables.find(function(token) {
-            console.log("token.data.name: " + token.data.name);
-            if(token.data.name.search(targetRegExp) !== -1) {
+        let targetTeleportal = canvas.tokens.placeables.find(function (token) {
+            if (token.data.name.search(targetRegExp) !== -1) {
                 return token;
             }
         });
-        console.log("targetTeleportal");
-        console.log(targetTeleportal);
 
         let targetX = targetTeleportal.data.x;
         let targetY = targetTeleportal.data.y;
 
         let tokenIndex = 0;
 
-        ownedTokens.forEach(token => {
+        let validOwnedTokenKeys = Object.keys(validOwnedTokens);
+        validOwnedTokenKeys.forEach(key => {
 
-            token.setPosition(targetX, targetY, false);
+            validOwnedTokens[key].setPosition(targetX, targetY, false);
 
             let correctedTargetX = targetX + validTargetOffset[tokenIndex][0];
             let correctedTargetY = targetY + validTargetOffset[tokenIndex][1];
-            token.update({
+            validOwnedTokens[key].update({
                 "x": correctedTargetX,
                 "y": correctedTargetY
-            }, {animate: false});
+            });
             tokenIndex++;
         })
 
-    } catch(e) {
-        console.log("No valid target teleportal found");
+    } catch (e) {
+        ui.notifications.warn("You hear a low vibrating hum, but nothing happens");
     }
-    
+
 
 }
