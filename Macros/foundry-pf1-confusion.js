0d7348fb4cee8b512ff4282afd8bd3f73326de4d
Lavaeolous <fallenshideout@gmx.de>
2022-01-19 11:33:38 +0100

    Updated to be compatible with v9 of FoundryVTT

--- Macros/foundry-pf1-confusion.js
+++ Macros/foundry-pf1-confusion.js
@@ -1,26 +1,36 @@
 // A very basic macro to roll for confusion and post its effects to chat
 
-let confusionRoll = new Roll('1d100').roll().result
+let confusionRoll = new Roll('1d100');
+confusionRoll.evaluate({"async": false});
 
 let confusionResult = ""
 
-if (confusionRoll <= 25) {
+if (confusionRoll.result <= 25) {
      // Act normally
-     confusionResult = "Act normally"
-} else if (confusionRoll <= 50) {
+     confusionResult = ", but acts normally."
+} else if (confusionRoll.result <= 50) {
      // Babbles incoherently
-     confusionResult = "Babble incoherently"
-} else if (confusionRoll <= 75) {
+     confusionResult = " and babbles incoherently."
+} else if (confusionRoll.result <= 75) {
      // Deals 1d8 points of damage + Str to self
-     confusionResult = "Deal 1d8+Str-Mod"
+     confusionResult = " and hits themself for [[1d8+@abilities.str.mod]] damage."
 } else {
      // Attacks nearest creature
-     confusionResult = "Attack nearest Creature"
+     confusionResult = " and attacks the nearest creature (friend or foe)."
 }
 
 let chatData = {
     //user: game.user._id,
     speaker: ChatMessage.getSpeaker(),
-    content: confusionResult,
+    content: `
+        <div class="pf1 chat-card item-card" data-actor-id="{{actor.id}}" data-item-id="{{item.id}}" {{#if tokenId}}data-token-id="{{tokenId}}"{{/if}}>
+          <header class="card-header flexrow">
+            <img src="assets/kingmaker%20icons/Confusion.png" title="Confusion" width="36" height="36"/>
+            <h3 class="item-name">Confusion</h3>
+          </header>
+
+        <div>${ChatMessage.getSpeaker().alias} is confused${confusionResult}</div>
+        </div>
+    `
 };
 ChatMessage.create(chatData, {});
---
+++ Macros/foundry-pf1-rollSkills.js
@@ -0,0 +1,181 @@
+const config = {
+  skills: ["knw", "acr", "apr", "blf", "clm", "crf", "dip", "dev", 
+          "dis", "esc", "fly", "han", "hea", "int", "lin", 
+          "per", "prf", "pro", "rid", "sen", "slt", "spl",
+          "ste", "sur", "swm", "umd"],
+  labels: ["Knowledge"],
+  blindrollSkills: ["apr", "blf", "dis", "dev", "apr", "per"],
+  withSubskills: ["art", "crf", "knw", "lor", "prf", "pro"],
+  unknownSkill: "Unknown Skill"
+};
+const css = `<style>
+.skill-dialog-box-buttons .dialog-buttons {
+  display: flex; 
+  flex-flow: row wrap; 
+}
+.skill-dialog-box-buttons .dialog-button {
+  flex: 1 0 33%;
+  width: auto;
+  padding: 2px 5px;
+}
+</style>`;
+
+async function _rollSkill(skillId, actor) {
+    
+    let rollMode = game.settings.get("core", "rollMode");
+    let blindRoll = false;
+
+    if (config.blindrollSkills.indexOf(skillId) !== -1) {
+        blindRoll = true;
+    } 
+
+    if (blindRoll) {
+        // Set rollMode to blindroll
+        game.settings.set("core", "rollMode", "blindroll");
+    }
+    
+    // Roll
+    await actor.rollSkill(skillId, { event: new MouseEvent({}), skipDialog: true } );
+
+    // Reset rollMode to previously saved rollMode
+    game.settings.set("core", "rollMode", rollMode);
+    
+
+};
+
+function actorsSkill(actors) {
+
+  const skillButtons = config.skills.reduce((buttons, skillId, index) => {
+    let label = config.labels[index];
+    if (label == null) 
+      label = `${CONFIG.PF1.skills[skillId]} ` || config.unknownSkill;
+    if (actors.length === 1 && !config.withSubskills.includes(skillId) && label !== config.unknownSkill) {
+      const skill = actors[0].getSkillInfo(skillId);
+      const mod = skill.mod;
+      label = label + (mod < 0 ? "" : "+") + mod;
+      if (skill.rt && skill.rank === 0) {
+        label = `<span style="color: red;">${label}</span>`;
+      }
+    }
+
+    let skill = {
+      id: skillId,
+      label: label
+    };
+
+    if (skillId === "knw")
+      skill.callback = () => actors.forEach(actor => actorKnowledge(actor));
+    else if (config.withSubskills.includes(skillId))
+      skill.callback = () => actors.forEach(actor => actorSubSkill(actor, label, skillId));
+    else 
+      skill.callback = () => actors.forEach(actor => _rollSkill(skillId, actor));
+    
+    buttons.push(skill);
+    return buttons;
+  }, []);
+  skillButtons.sort((skill1, skill2) => skill1.id.localeCompare(skill2.id));
+
+  const msg = `Choose a skill to roll for the following actor(s): <strong>${actors.map(o => o.name).join("</strong>, <strong>")}</strong>`;
+    new Dialog({
+        title: "Roll skill", 
+        content: `<p>${msg}</p>` + css,
+        buttons: skillButtons ,
+    }, {
+      classes: ["skill-dialog-box-buttons"],
+      resizable: true,
+    }).render(true);   
+
+}
+
+function actorSubSkill(actor, skillName, skill) {
+
+  const subSkills = actor.getSkillInfo(skill).subSkills;
+  const subSkillIds = Object.keys(subSkills).filter(name => name.startsWith(skill))
+  if (!subSkillIds.length) {
+    return;
+  }
+
+  let subSkillButtons = [];
+  for (const sub of subSkillIds) {
+    subSkillButtons.push({
+      label: `${subSkills[sub].name} +${subSkills[sub].mod}`,
+      callback: () => _rollSkill(`${skill}.subSkills.${sub}`, actor),
+    });
+  }
+
+  const msg = `Choose a ${skillName} skill roll for <strong>${actor.name}</strong>`
+    new Dialog({
+        title: `Roll ${skillName} skill`, 
+        content: `<p>${msg}</p>` + css,
+        buttons: subSkillButtons ,
+    }, {
+      classes: ["skill-dialog-box-buttons"],
+    }).render(true);
+}
+
+function actorKnowledge(actor) {
+
+  const knowledges = ["kar","kdu", "ken", "kge", "khi",
+    "klo", "kna", "kno", "kpl", "kre"];
+
+    const knowledgeButtons = knowledges.reduce((buttons, skillId) => {
+      let label = `${CONFIG.PF1.skills[skillId]} `;
+      const skill = actor.getSkillInfo(skillId);
+      const mod = skill.mod;
+      label = label + (mod < 0 ? "" : "+") + mod;
+      if (skill.rt && skill.rank === 0) {
+        label = `<span style="color: red;">${label}</span>`;
+      }
+    
+      buttons[skillId] = {
+        label: label,
+        callback: () => _rollSkill(skillId, actor),
+      };
+      return buttons;
+    }, {});
+
+    const msg = `Choose a knowledge skill roll for <strong>${actor.name}</strong>`
+    new Dialog({
+        title: `Roll knowledge skill`, 
+        content: `<p>${msg}</p>` + css,
+        buttons: knowledgeButtons,
+    }, {
+      classes: ["skill-dialog-box-buttons"],
+    }).render(true);
+}
+
+// ---- Starts here
+
+const tokens = canvas.tokens.controlled;
+const selected = (!tokens.length ? false : true);
+
+
+let actors = tokens.map(o => o.actor);
+
+if (!actors.length) {
+  actors = game.actors.filter(o => o.isPC && o.isOwner == true);
+}
+actors = actors.filter(o => o.isOwner == true);
+
+if (!actors.length) 
+  ui.notifications.warn("No applicable actor(s) found");
+else {
+
+  if (selected || actors.length === 1) {
+    actorsSkill(actors)
+  }
+  else if (actors.length > 1) {
+      const actorButtons = actors.map(actor => ({label: actor.name, callback: () => actorsSkill([actor])}));
+  
+      const msg = "Choose an actor for your skill roll";
+  
+      new Dialog({
+        title: "Choose an actor",
+        content: `<p>${msg}</p>` + css,
+        buttons: actorButtons,
+      }, {
+        classes: ["skill-dialog-box-buttons"],
+      }).render(true);
+  }
+
+}
